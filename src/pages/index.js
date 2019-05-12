3ddbd4c71562fc8e643eb479a931067261ee0825
kurt miller <kmiller9393@gmail.com>
2019-05-11 21:15:36 -0600

    Add visible posts to home page that user can click on to view more info

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { withRouteData } from 'react-static';
+import { Link } from '@reach/router';
 
-export default () => (
-  <div style={{ textAlign: 'center' }}>
-    <h1>Welcome to React-Static</h1>
+export default withRouteData(({ posts }) => (
+  <div className="container">
+    {posts.map(post => (
+      <Link key={post.id} to={`/post/${post.id}`} className="card">
+        <div style={{ paddingRight: '1rem' }}>
+          <img
+            alt={post.title}
+            className="card-img"
+            src={`https://media.graphcms.com/resize=w:170,h:150,fit:crop/${post.image.handle}`}
+          />
+        </div>
+        <div>
+          <h3 style={{ margin: 0 }}>{post.title}</h3>
+          <p style={{ display: 'flex' }}>
+            <img src={'https://icon.now.sh/face/2386ea'} alt="author" />
+            {post.author.name}
+          </p>
+          {post.tags.map(tag => (
+            <span className="card-tag" key={tag}>
+              {tag}
+            </span>
+          ))}
+        </div>
+      </Link>
+    ))}
   </div>
-)
+));
