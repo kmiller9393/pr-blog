77419a9c35b1bd320e7dd4eb048f06bddaa9fb8b
kurt miller <kmiller9393@gmail.com>
2019-05-11 19:09:33 -0600

    Add graphcms endpoint, query and update static.config.js

---
+++ static.config.js
@@ -0,0 +1,63 @@
+import { request } from 'graphql-request';
+import path from 'path';
+
+const GRAPHCMS_ENDPOINT = 'https://api-uswest.graphcms.com/v1/cjvjziu5s627901ehblbcdtu9/master';
+
+const query = `
+{
+  posts{
+    id
+    title
+    image{
+      handle
+    }
+    content
+    tags
+    author{
+      id
+      name
+    }
+  }
+  
+  authors{
+    id
+    name
+    avatar{
+      handle
+    }
+    bibliography
+  }
+}
+`
+
+export default {
+  getRoutes: async () => {
+    const { authors, posts } = await request(GRAPHCMS_ENDPOINT, query)
+
+    return [
+      {
+        path: '/',
+        getData: () => ({
+          posts
+        }),
+        children: posts.map(post => ({
+          path: `/post/${post.id}`,
+          template: 'src/containers/Post',
+          getData: () => ({
+            post
+          })
+        }))
+      }
+    ];
+  },
+  plugins: [
+    [
+      require.resolve('react-static-plugin-source-filesystem'),
+      {
+        location: path.resolve('./src/pages')
+      }
+    ],
+    require.resolve('react-static-plugin-reach-router'),
+    require.resolve('react-static-plugin-sitemap')
+  ]
+};
