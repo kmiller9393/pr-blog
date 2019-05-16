b28c2e85922ae1d28ad3adc894eff133ab35a6ae
kurt miller <kmiller9393@gmail.com>
2019-05-15 22:01:00 -0600

    Update homepage styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import { Root, Routes, addPrefetchExcludes } from 'react-static';
+import { Root, Routes } from 'react-static';
 import { Router } from 'components/Router';
-import Dynamic from 'containers/Dynamic';
 import Header from 'components/Header';
 import './app.css';
-
-// Any routes that start with 'dynamic' will be treated as non-static routes
-addPrefetchExcludes(['dynamic'])
 
 const App = () => {
   return (
@@ -15,13 +11,12 @@ const App = () => {
         <Header />
         <div className="content">
           <Router>
-            <Dynamic path="dynamic" />
             <Routes path="*" />
           </Router>
         </div>
       </React.Suspense>
     </Root>
-  )
-}
+  );
+};
 
-export default App
+export default App;
