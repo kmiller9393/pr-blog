3c520d94d80f9dddb558d571289096e19dcd1fea
kurt miller <kmiller9393@gmail.com>
2019-05-15 22:01:36 -0600

    Update about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,17 +3,16 @@ import { withRouteData } from 'react-static';
 
 export default withRouteData(({ authors }) => (
   <div>
-    <h1 className="about-header">About Me</h1>
     {authors.map(author => (
       <div className="about-author" key={author.id}>
-        <div className="about-header">
-          <img
-            className="about-avatar"
-            alt={author.name}
-            src={`https://media.graphcms.com/resize=w:100,h:100,fit:crop/${author.avatar.handle}`}
-          />
-          <h2>Hello my name is {author.name}</h2>
-        </div>
+        <img
+          className="about-avatar"
+          alt={author.name}
+          src={`https://media.graphcms.com/resize=w:100,h:100,fit:crop/${
+            author.avatar.handle
+          }`}
+        />
+        <h2>Hello my name is {author.name}</h2>
         <p style={{ textAlign: 'center' }}>{author.bibliography}</p>
       </div>
     ))}
