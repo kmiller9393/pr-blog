dee8eb6daeb3d301bb94e838779fc0d284663227
kurt miller <kmiller9393@gmail.com>
2019-05-15 22:02:35 -0600

    Update post page

--- src/pages/post.js
+++ src/pages/post.js
@@ -3,14 +3,16 @@ import { withRouteData } from 'react-static';
 import Markdown from 'react-markdown';
 
 export default withRouteData(({ post }) => (
-    <article>
-        <h1>{post.title}</h1>
-        <div className="placeholder">
-            <img
-                alt={post.title}
-                src={`https://media.graphcms.com/resize=w:650,h:366,fit:crop/${post.image.handle}`}
-            />
-        </div>
-        <Markdown source={post.content} />
-    </article>
+  <article>
+    <h1>{post.title}</h1>
+    <div className="placeholder">
+      <img
+        alt={post.title}
+        src={`https://media.graphcms.com/resize=w:650,h:366,fit:crop/${
+          post.image.handle
+        }`}
+      />
+    </div>
+    <Markdown source={post.content} />
+  </article>
 ));
