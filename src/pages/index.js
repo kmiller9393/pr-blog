127d7acf3061f1be883fa86097b33c7dd6a4f516
kurt miller <kmiller9393@gmail.com>
2019-05-15 22:02:23 -0600

    Update homepage to pull data from backend

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,17 @@
 import React from 'react';
 import { withRouteData } from 'react-static';
-import { Link } from '@reach/router';
 
-export default withRouteData(({ posts }) => (
+export default withRouteData(({ homes }) => (
   <div className="container">
-    {posts.map(post => (
-      <Link key={post.id} to={`/post/${post.id}`} className="card">
-        <div style={{ paddingRight: '1rem' }}>
-          <img
-            alt={post.title}
-            className="card-img"
-            src={`https://media.graphcms.com/resize=w:170,h:150,fit:crop/${post.image.handle}`}
-          />
-        </div>
-        <div>
-          <h3 style={{ margin: 0 }}>{post.title}</h3>
-          <p style={{ display: 'flex' }}>
-            <img src={'https://icon.now.sh/face/2386ea'} alt="author" />
-            {post.author.name}
-          </p>
-          {post.tags.map(tag => (
-            <span className="card-tag" key={tag}>
-              {tag}
-            </span>
-          ))}
-        </div>
-      </Link>
+    {homes.map(home => (
+      <div className="image-container">
+        <img
+          className="home-image"
+          src={`https://media.graphcms.com/${home.image.handle}`}
+          alt="kimaleen tran"
+        />
+        <h2>{home.opening}</h2>
+      </div>
     ))}
   </div>
 ));
