b41d2629576fa3b214188b88b8c43755646339fd
kurt miller <kmiller9393@gmail.com>
2019-05-18 23:41:11 -0600

    Add initial Footer to homepage

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+
+export default () => (
+  <section className="footer">
+    <p>Link</p>
+    <p>Link</p>
+    <p>Link</p>
+    <p>Link</p>
+  </section>
+);
