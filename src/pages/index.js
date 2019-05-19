4751123d224a442545acc3c33f233520bc487439
kurt miller <kmiller9393@gmail.com>
2019-05-18 23:42:21 -0600

    Update homepage, add new banner, styling, instagram posts and footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { withRouteData } from 'react-static';
+import RecentPosts from '../components/RecentPosts/RecentPosts';
+import Footer from '../components/Footer/Footer';
 
-export default withRouteData(({ homes }) => (
-  <div className="container">
-    {homes.map(home => (
-      <div className="image-container">
-        <img
-          className="home-image"
-          src={`https://media.graphcms.com/${home.image.handle}`}
-          alt="kimaleen tran"
-        />
-        <h2>{home.opening}</h2>
-      </div>
-    ))}
-  </div>
-));
+export default () => (
+  <section className="home-container">
+    <article className="image-container">
+      <div className="banner-image" />
+      <h2 className="banner-text">I Love Experiencing Special Moments</h2>
+      <h2 className="banner-text-1">And Helping People Do The Same.</h2>
+    </article>
+    <RecentPosts />
+    <Footer />
+  </section>
+);
---
+++ src/utils/instaPosts.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import InstagramEmbed from 'react-instagram-embed';
+
+const instaPosts = [
+  <InstagramEmbed
+    url="https://www.instagram.com/p/BxbSE6vhC7Y/"
+    maxWidth={300}
+    hideCaption={true}
+    containerTagName="div"
+  />,
+  <InstagramEmbed
+    url="https://www.instagram.com/p/BxQ7QVJhPty/"
+    maxWidth={300}
+    hideCaption={true}
+    containerTagName="div"
+  />,
+  <InstagramEmbed
+    url="https://www.instagram.com/p/BwDtB7nBupK/"
+    maxWidth={300}
+    hideCaption={true}
+    containerTagName="div"
+  />,
+  <InstagramEmbed
+    url="https://www.instagram.com/p/Bv24WsJBagt/"
+    maxWidth={300}
+    hideCaption={true}
+    containerTagName="div"
+  />,
+  <InstagramEmbed
+    url="https://www.instagram.com/p/Bvk7FlUBMQ3/"
+    maxWidth={300}
+    hideCaption={true}
+    containerTagName="div"
+  />,
+  <InstagramEmbed
+    url="https://www.instagram.com/p/BuVGrpwBg96/"
+    maxWidth={300}
+    hideCaption={true}
+    containerTagName="div"
+  />
+];
+
+export default instaPosts;
