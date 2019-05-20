8686c083095ebf979951fa37a16395985a61f86c
kurt miller <kmiller9393@gmail.com>
2019-05-19 20:25:35 -0600

    Update Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Footer.css';
+import { withRouteData } from 'react-static';
 
-export default () => (
+export default withRouteData(({ post }) => (
   <section className="footer">
-    <p>Link</p>
-    <p>Link</p>
-    <p>Link</p>
-    <p>Link</p>
+    <h5 className="footer-header">Popular Posts</h5>
+    <h5 className="footer-header">Weekly Email List</h5>
+    <h5 className="footer-header">Contact</h5>
   </section>
-);
+));
