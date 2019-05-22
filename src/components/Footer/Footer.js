661062edc397fc1dc7f69219f3432396cbeff78c
kurt miller <kmiller9393@gmail.com>
2019-05-21 19:31:56 -0600

    Add EmailForm to Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Footer.css';
 import { withRouteData } from 'react-static';
+import EmailForm from '../EmailForm/EmailForm';
 
-export default withRouteData(({ post }) => (
-  <section className="footer">
-    <h5 className="footer-header">Popular Posts</h5>
-    <h5 className="footer-header">Weekly Email List</h5>
-    <h5 className="footer-header">Contact</h5>
-  </section>
-));
+export default class Footer extends Component {
+  render() {
+    return (
+      <section className="footer">
+        <h5 className="footer-header">Popular Posts</h5>
+        <div className="weekly-email">
+          <h3 className="footer-header">Weekly Email List</h3>
+          <EmailForm addIdea={this.props.addIdea} />
+        </div>
+        <h5 className="footer-header">Contact</h5>
+      </section>
+    );
+  }
+}
