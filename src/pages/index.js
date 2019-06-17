9235309faed6653557090bb76ff3ad89eb4ca205
kurt miller <kmiller9393@gmail.com>
2019-06-17 12:17:23 -0600

    Update index.js, add RecentPosts component and Footer component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { withRouteData } from 'react-static';
 import RecentPosts from '../components/RecentPosts/RecentPosts';
+import InstaPost from '../components/InstaPost/InstaPost';
 import Footer from '../components/Footer/Footer';
 
-export default () => (
-  <section className="home-container">
-    <article className="image-container">
-      <div className="banner-image" />
-      <h2 className="banner-text">I Love Experiencing Special Moments</h2>
-      <h2 className="banner-text-1">And Helping People Do The Same.</h2>
-    </article>
-    <RecentPosts />
-    <Footer />
-  </section>
-);
+class index extends Component {
+  constructor() {
+    super();
+    this.state = {
+      ideas: []
+    };
+  }
+
+  addIdea = (name, email, idea) => {
+    const newIdea = { ...idea, id: Date.now() };
+    const ideas = [...this.state.ideas, newIdea];
+    this.setState({ ideas });
+  };
+
+  render() {
+    const { handle } = this.props.authors[0].avatar;
+    const { bibliography } = this.props.authors[0];
+
+    return (
+      <div className="home-container">
+        <article className="image-container banner-image">
+          <aside className="author-image-container">
+            <img
+              className="author-image"
+              src={`https://media.graphcms.com/${handle}`}
+              alt="Kimaleen"
+            />
+          </aside>
+          <aside className="bio-text">
+            <p>{bibliography}</p>
+            <h4 className="signature">Kimaleen</h4>
+          </aside>
+        </article>
+        <RecentPosts />
+        <Footer addIdea={this.addIdea} />
+      </div>
+    );
+  }
+}
+
+export default withRouteData(index);
