c114d011a099089d0a466d81b56ba1c752aaf6dd
kurt miller <kmiller9393@gmail.com>
2019-06-17 12:14:54 -0600

    Add Navbar component to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Root, Routes } from 'react-static';
 import { Router } from './components/Router/Router';
 import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
 import './app.css';
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <Root>
       <React.Suspense fallback={<em>Loading...</em>}>
         <Header />
+        <Navbar />
         <div className="content">
           <Router>
             <Routes path="*" />
