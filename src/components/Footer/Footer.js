91a777ca0046760b9938e25e8f6a030a4c2c8a9d
kurt miller <kmiller9393@gmail.com>
2019-06-17 20:24:14 -0600

    Update Footer component and styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import './Footer.css';
-import { withRouteData } from 'react-static';
 import EmailForm from '../EmailForm/EmailForm';
 
 export default class Footer extends Component {
   render() {
     return (
       <section className="footer">
-        <div>
-          <h3 className="footer-header">Popular Posts</h3>
-        </div>
         <div className="weekly-email">
-          <h3 className="footer-header">Weekly Email List</h3>
+          <h3 className="footer-header">Subscribe to Receive Updates</h3>
           <EmailForm addIdea={this.props.addIdea} />
         </div>
       </section>
