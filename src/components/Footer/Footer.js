39cbfd7d260b1185463a3a0ff9407f5ea03d8b80
kurt miller <kmiller9393@gmail.com>
2019-06-18 21:20:01 -0600

    Add social media icons to Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import './Footer.css';
 import EmailForm from '../EmailForm/EmailForm';
+import instagram from '../../images/instagram-light.svg';
+import facebook from '../../images/facebook-light.svg';
+import linkedin from '../../images/linkedin-light.svg';
+import './Footer.css';
 
 export default class Footer extends Component {
   render() {
@@ -9,6 +12,29 @@ export default class Footer extends Component {
         <div className="weekly-email">
           <h3 className="footer-header">Subscribe to Receive Updates</h3>
           <EmailForm addIdea={this.props.addIdea} />
+        </div>
+        <div className="footer-link-container">
+          <a
+            href="https://www.instagram.com/kimaleentran/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="social-image" src={instagram} alt="instagram" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/kimaleentran/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="social-image" src={linkedin} alt="linkedin" />
+          </a>
+          <a
+            href="https://www.facebook.com/kimaleen.tran.3"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="social-image" src={facebook} alt="facebook" />
+          </a>
         </div>
       </section>
     );
