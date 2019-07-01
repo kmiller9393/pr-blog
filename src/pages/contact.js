df5ea6ef66d583c2c77bf7952877352c072a1eac
kurt miller <kmiller9393@gmail.com>
2019-06-30 20:20:21 -0600

    Add contact.js to serve as template of contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ContactForm from '../components/ContactForm/ContactForm';
+
+export default () => (
+  <div className="contact-container">
+    <h2 className="contact-header">Let's Connect</h2>
+    <ContactForm />
+  </div>
+);
