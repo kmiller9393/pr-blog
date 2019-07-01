0a0ace3fca53cb3aa56123562097067a09a7c3cb
kurt miller <kmiller9393@gmail.com>
2019-06-30 20:19:23 -0600

    Add contact page and update contact page styling

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -6,6 +6,7 @@ export default {
   '../src/pages/404.js': require('../src/pages/404.js').default,
 '../src/pages/about.js': require('../src/pages/about.js').default,
 '../src/pages/blog.js': require('../src/pages/blog.js').default,
+'../src/pages/contact.js': require('../src/pages/contact.js').default,
 '../src/pages/fashion.js': require('../src/pages/fashion.js').default,
 '../src/pages/food.js': require('../src/pages/food.js').default,
 '../src/pages/index.js': require('../src/pages/index.js').default,
