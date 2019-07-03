650194d9c5bb8860b97efecff34279adad97a2f7
kurt miller <kmiller9393@gmail.com>
2019-07-02 21:08:26 -0600

    Update ContactForm styling

--- artifacts/react-static-templates.js
+++ artifacts/react-static-templates.js
@@ -7,9 +7,9 @@ export default {
 '../src/pages/about.js': require('../src/pages/about.js').default,
 '../src/pages/blog.js': require('../src/pages/blog.js').default,
 '../src/pages/contact.js': require('../src/pages/contact.js').default,
-'../src/pages/fashion.js': require('../src/pages/fashion.js').default,
 '../src/pages/food.js': require('../src/pages/food.js').default,
 '../src/pages/index.js': require('../src/pages/index.js').default,
+'../src/pages/lifestyle.js': require('../src/pages/lifestyle.js').default,
 '../src/pages/post.js': require('../src/pages/post.js').default,
 '../src/pages/travel.js': require('../src/pages/travel.js').default,
 '../src/pages/post': require('../src/pages/post').default
