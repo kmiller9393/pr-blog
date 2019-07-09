b13333e6f339fe0a299f2086cb9177c07b48e8b4
kurt miller <kmiller9393@gmail.com>
2019-07-08 21:09:17 -0600

    Add Helmet component to App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import { Root, Routes } from 'react-static';
 import { Router } from './components/Router/Router';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import { Helmet } from 'react-helmet';
+import initEmailjs from './utils/initEmailjs';
 import './app.css';
 
 const App = () => {
   return (
     <Root>
+      <Helmet>
+        <title>Kimaleen Tran</title>
+        <script
+          type="text/javascript"
+          src="https://cdn.emailjs.com/sdk/2.3.2/email.min.js"
+          onLoad={initEmailjs()}
+        />
+      </Helmet>
       <React.Suspense fallback={<em>Loading...</em>}>
         <Header />
         <Navbar />
