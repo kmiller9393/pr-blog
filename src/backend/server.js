3dbecf83719b6a00926daacd50471817343babb4
kurt miller <kmiller9393@gmail.com>
2019-07-21 21:09:29 -0600

    Add server.js, add .env configuration and connect backend to frontend

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,13 +1,22 @@
 const bodyParser = require('body-parser');
 const apiRouter = require('./routes');
+const path = require('path');
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 
+let p = path.join(__dirname, '../../dist');
+
+app.use(express.static(p));
+
 app.use(bodyParser.json());
+
 app.use(apiRouter);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
