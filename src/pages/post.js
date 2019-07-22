bb568b26263e1ef22e8ea53a50433d119533b616
kurt miller <kmiller9393@gmail.com>
2019-07-21 21:21:57 -0600

    Update post.js to only render title, image and  content when all are available

--- src/pages/post.js
+++ src/pages/post.js
@@ -4,17 +4,19 @@ import Markdown from 'react-markdown';
 
 export default withRouteData(({ post }) => (
   <article className="blog-card">
-    <h1 className="post-main-title ">{post.title}</h1>
-    <div className="placeholder">
-      <img
-        alt={post.title}
-        src={`https://media.graphcms.com/resize=w:650,h:366,fit:crop/${
-          post.image[0].handle
-        }`}
-      />
-    </div>
+    {post && <h1 className="post-main-title ">{post.title}</h1>}
+    {post && (
+      <div className="placeholder">
+        <img
+          alt={post.title}
+          src={`https://media.graphcms.com/resize=w:650,h:366,fit:crop/${
+            post.image[0].handle
+          }`}
+        />
+      </div>
+    )}
     <div className="blog-content">
-      <Markdown source={post.content} />
+      {post && <Markdown source={post.content} />}
     </div>
   </article>
 ));
