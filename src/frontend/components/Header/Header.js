604792443ac45014c98fd1904c3f1224e034a9c3
kurt miller <kmiller9393@gmail.com>
2019-07-21 21:10:43 -0600

    Update withRouteData from Header.js

---
+++ src/frontend/components/Header/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from '../Router/Router';
+import instagram from '../../images/instagram.svg';
+import facebook from '../../images/facebook.svg';
+import linkedin from '../../images/linkedin.svg';
+import './Header.css';
+
+export default () => (
+  <header className="home-header">
+    <div className="title-link-container">
+      <div />
+      <div>
+        <h1 className="header-h1">
+          <Link className="main-header-link" to="/">
+            KIMALEEN
+          </Link>
+        </h1>
+      </div>
+      <div className="header-link-container">
+        <a
+          href="https://www.instagram.com/kimaleentran/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="social-image" src={instagram} alt="instagram" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/kimaleentran/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="social-image" src={linkedin} alt="linkedin" />
+        </a>
+        <a
+          href="https://www.facebook.com/kimaleen.tran.3"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="social-image" src={facebook} alt="facebook" />
+        </a>
+      </div>
+    </div>
+  </header>
+);
