bb4fd52347eb459c299617bd50a4e242a2fee57c
kurt miller <kmiller9393@gmail.com>
2019-07-21 21:08:48 -0600

    Create routes.js, refactor to move sendEmail method into utils directory

--- src/backend/routes.js
+++ src/backend/routes.js
@@ -1,39 +1,15 @@
 const express = require('express');
-const mailgun = require('mailgun-js');
+const sendEmail = require('./utils/sendEmail');
 
 const router = express.Router();
-
-router.get('/', (req, res, next) => {
-  res.json('World');
-});
-
-function sendEmail(senderEmail, receiverEmail, subject, feedback) {
-  const data = {
-    from: senderEmail,
-    to: receiverEmail,
-    subject,
-    text: feedback
-  };
-
-  const mg = mailgun({
-    apiKey: 'ea4ee815b6abac63082a87b2319341c8-2b0eef4c-ba19fc6b',
-    domain: 'sandboxe541cb548c3e41fe8ab3a7c16b600fdb.mailgun.org'
-  });
-
-  mg.messages().send(data, function(error, body) {
-    console.log(body);
-
-    if (error) console.log(error, error.message);
-  });
-}
 
 router.post('/api/contact', async (req, res, next) => {
   try {
     await sendEmail(
-      'kmiller9393@gmail.com',
-      'kmiller9393@gmail.com',
-      'HERE IS THE SUBJECT',
-      'the body of text here'
+      req.body.email,
+      process.env.EMAIL_RECEIVER,
+      req.body.subject,
+      req.body.message
     );
     res.send('Email sent!');
   } catch (err) {
