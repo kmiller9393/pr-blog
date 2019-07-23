f52d1940557992ea0c0c603cedee5a3f88473a4c
kurt miller <kmiller9393@gmail.com>
2019-07-22 21:26:06 -0600

    Update ContactForm background and add Footer

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ContactForm from '../frontend/components/ContactForm/ContactForm';
+import Footer from '../frontend/components/Footer/Footer';
 
 export default () => (
   <div className="contact-container">
-    <h2 className="contact-header">Let's Connect</h2>
-    <ContactForm />
+    <div className="contact-form-container">
+      <h2 className="contact-header">Let's Connect</h2>
+      <ContactForm />
+    </div>
+    <Footer />
   </div>
 );
