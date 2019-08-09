4433b3006c8ccc5f7d179af96cb47f74003724ee
kurt miller <kmiller9393@gmail.com>
2019-08-08 21:46:11 -0600

    Update Header styling and add DrawerButton to Header

--- src/frontend/components/Header/Header.js
+++ src/frontend/components/Header/Header.js
@@ -1,44 +1,51 @@
 import React from 'react';
 import { Link } from '../Router/Router';
+import DrawerButton from '../DrawerButton/DrawerButton';
 import instagram from '../../images/instagram.svg';
 import facebook from '../../images/facebook.svg';
 import linkedin from '../../images/linkedin.svg';
 import './Header.css';
 
-export default () => (
-  <header className="home-header">
-    <div className="title-link-container">
-      <div className="header-link-container">
-        <a
-          href="https://www.instagram.com/kimaleentran/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img className="social-image" src={instagram} alt="instagram" />
-        </a>
-        <a
-          href="https://www.linkedin.com/in/kimaleentran/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img className="social-image" src={linkedin} alt="linkedin" />
-        </a>
-        <a
-          href="https://www.facebook.com/kimaleen.tran.3"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img className="social-image" src={facebook} alt="facebook" />
-        </a>
+const Header = props => {
+  return (
+    <header className="home-header">
+      <div className="title-link-container">
+        <div className="header-link-container">
+          <a
+            href="https://www.instagram.com/kimaleentran/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="social-image" src={instagram} alt="instagram" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/kimaleentran/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="social-image" src={linkedin} alt="linkedin" />
+          </a>
+          <a
+            href="https://www.facebook.com/kimaleen.tran.3"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img className="social-image" src={facebook} alt="facebook" />
+          </a>
+        </div>
+        <div>
+          <h1 className="header-h1">
+            <Link className="main-header-link" to="/">
+              KIMALEEN
+            </Link>
+          </h1>
+        </div>
+        <div>
+          <DrawerButton toggleDrawer={props.toggleDrawer} />
+        </div>
       </div>
-      <div>
-        <h1 className="header-h1">
-          <Link className="main-header-link" to="/">
-            KIMALEEN
-          </Link>
-        </h1>
-      </div>
-      <div />
-    </div>
-  </header>
-);
+    </header>
+  );
+};
+
+export default Header;
