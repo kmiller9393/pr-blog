5d206f498fe503316b5d4e80328ea7e6d8c603e2
kurt miller <kmiller9393@gmail.com>
2019-08-08 21:45:11 -0600

    Remove Header and Navbar components from App.js

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Root, Routes } from 'react-static';
 import { Router } from './components/Router/Router';
-import Header from './components/Header/Header';
-import Navbar from './components/Navbar/Navbar';
 import { Helmet } from 'react-helmet';
 import './app.css';
 
@@ -13,8 +11,6 @@ const App = () => {
         <title>Kimaleen Tran</title>
       </Helmet>
       <React.Suspense fallback={<em>Loading...</em>}>
-        <Header />
-        <Navbar />
         <div className="content">
           <Router>
             <Routes path="*" />
