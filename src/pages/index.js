db90ae59e7e2255700a23fb2c4957aabb745e4bd
kurt miller <kmiller9393@gmail.com>
2019-08-13 19:01:37 -0600

    Remove toggleDrawer logic from index page into Navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,12 @@ import React, { Component } from 'react';
 import { withRouteData } from 'react-static';
 import RecentPosts from '../frontend/components/RecentPosts/RecentPosts';
 import Footer from '../frontend/components/Footer/Footer';
-import Header from '../frontend/components/Header/Header';
 import Navbar from '../frontend/components/Navbar/Navbar';
-import Sidebar from '../frontend/components/Sidebar/Sidebar';
-import Backdrop from '../frontend/components/Backdrop/Backdrop';
 import signature from '../frontend/images/signature.svg';
 
 class index extends Component {
   state = {
-    ideas: [],
-    drawerOpen: false
+    ideas: []
   };
 
   getPosts = () => {
@@ -33,25 +29,14 @@ class index extends Component {
     this.setState({ ideas });
   };
 
-  toggleDrawer = () => {
-    this.setState(prevState => {
-      return { drawerOpen: !prevState.drawerOpen };
-    });
-  };
-
   render() {
     const { handle } = this.props.authors[0].avatar;
     const { bibliography } = this.props.authors[0];
-    const { drawerOpen } = this.state;
-    const { toggleDrawer } = this;
 
     const recentPosts = this.getPosts();
 
     return (
       <div className="home-container">
-        <Sidebar display={drawerOpen} />
-        {drawerOpen && <Backdrop toggleDrawer={toggleDrawer} />}
-        <Header toggleDrawer={toggleDrawer} />
         <Navbar />
         <article className="image-container banner-image">
           <aside className="author-image-container">
