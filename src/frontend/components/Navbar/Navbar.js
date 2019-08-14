bbf18418ee69d3be8bc0f2935b2063ecf1323bfb
kurt miller <kmiller9393@gmail.com>
2019-08-13 19:00:25 -0600

    Move drawerOpen logic for menu button into Navbar component

--- src/frontend/components/Navbar/Navbar.js
+++ src/frontend/components/Navbar/Navbar.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Link } from '../Router/Router';
 import { toggleStickyNav } from '../../utils/toggleStickyNav.js';
 import { goToTop } from 'react-scrollable-anchor';
+import Header from '../Header/Header';
+import Sidebar from '../Sidebar/Sidebar';
+import Backdrop from '../Backdrop/Backdrop';
 import './Navbar.css';
 
 const isActive = ({ isCurrent }) => {
@@ -9,6 +12,10 @@ const isActive = ({ isCurrent }) => {
 };
 
 export default class Navbar extends Component {
+  state = {
+    drawerOpen: false
+  };
+
   componentDidMount() {
     window.addEventListener('scroll', this.handleNav);
   }
@@ -17,73 +24,87 @@ export default class Navbar extends Component {
     toggleStickyNav();
   };
 
+  toggleDrawer = () => {
+    this.setState(prevState => {
+      return { drawerOpen: !prevState.drawerOpen };
+    });
+  };
+
   render() {
+    const { toggleDrawer } = this;
+    const { drawerOpen } = this.state;
+
     return (
       <nav className="header-nav">
-        <span className="logo">K</span>
-        <Link
-          getProps={isActive}
-          className="header-link"
-          id="home"
-          to="/"
-          onClick={goToTop}
-        >
-          Home
-        </Link>
-        <Link
-          getProps={isActive}
-          className="header-link"
-          id="blog"
-          to="/blog"
-          onClick={goToTop}
-        >
-          Blog
-        </Link>
-        <Link
-          getProps={isActive}
-          className="header-link"
-          id="thoughts"
-          to="/thoughts"
-          onClick={goToTop}
-        >
-          Thoughts
-        </Link>
-        <Link
-          getProps={isActive}
-          className="header-link"
-          id="lifestyle"
-          to="/lifestyle"
-          onClick={goToTop}
-        >
-          Life + Style
-        </Link>
-        <Link
-          getProps={isActive}
-          className="header-link"
-          id="travel"
-          to="/travel"
-          onClick={goToTop}
-        >
-          Travel
-        </Link>
-        <Link
-          getProps={isActive}
-          className="header-link"
-          id="food"
-          to="/food"
-          onClick={goToTop}
-        >
-          Food
-        </Link>
-        <Link
-          getProps={isActive}
-          className="header-link"
-          id="contact"
-          to="/contact"
-          onClick={goToTop}
-        >
-          Contact
-        </Link>
+        <Sidebar display={drawerOpen} />
+        {drawerOpen && <Backdrop toggleDrawer={toggleDrawer} />}
+        <Header toggleDrawer={toggleDrawer} />
+        <div>
+          <span className="logo">K</span>
+          <Link
+            getProps={isActive}
+            className="header-link"
+            id="home"
+            to="/"
+            onClick={goToTop}
+          >
+            Home
+          </Link>
+          <Link
+            getProps={isActive}
+            className="header-link"
+            id="blog"
+            to="/blog"
+            onClick={goToTop}
+          >
+            Blog
+          </Link>
+          <Link
+            getProps={isActive}
+            className="header-link"
+            id="thoughts"
+            to="/thoughts"
+            onClick={goToTop}
+          >
+            Thoughts
+          </Link>
+          <Link
+            getProps={isActive}
+            className="header-link"
+            id="lifestyle"
+            to="/lifestyle"
+            onClick={goToTop}
+          >
+            Life + Style
+          </Link>
+          <Link
+            getProps={isActive}
+            className="header-link"
+            id="travel"
+            to="/travel"
+            onClick={goToTop}
+          >
+            Travel
+          </Link>
+          <Link
+            getProps={isActive}
+            className="header-link"
+            id="food"
+            to="/food"
+            onClick={goToTop}
+          >
+            Food
+          </Link>
+          <Link
+            getProps={isActive}
+            className="header-link"
+            id="contact"
+            to="/contact"
+            onClick={goToTop}
+          >
+            Contact
+          </Link>
+        </div>
       </nav>
     );
   }
