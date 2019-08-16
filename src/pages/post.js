760291c1b8c277dd1592830db7d529bfbd855d64
kurt miller <kmiller9393@gmail.com>
2019-08-15 22:17:41 -0600

    Make blog post pages fully responsive

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { withRouteData } from 'react-static';
 import Markdown from 'react-markdown';
+import Navbar from '../frontend/components/Navbar/Navbar';
+import Footer from '../frontend/components/Footer/Footer';
 
 export default withRouteData(({ post }) => (
-  <article className="blog-card">
+  <article className="blog-post">
+    <Navbar />
     {post && <h1 className="post-main-title">{post.title}</h1>}
     {post && (
-      <div className="placeholder">
-        <img
-          alt={post.title}
-          src={`https://media.graphcms.com/resize=w:650,h:366,fit:crop/${
-            post.image[0].handle
-          }`}
-        />
-      </div>
+      <img
+        alt={post.title}
+        className="post-image"
+        src={`https://media.graphcms.com/${post.image[0].handle}`}
+      />
     )}
     <div className="blog-content">
       {post && <Markdown source={post.content} />}
     </div>
+    <Footer />
   </article>
 ));
