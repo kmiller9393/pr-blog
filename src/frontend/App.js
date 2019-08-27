364d5160cd3164ef41f5cb62203f09156d0f8cf0
kurt miller <kmiller9393@gmail.com>
2019-08-26 21:36:39 -0600

    Wrap application in new graphql-hooks provider and update GraphQLClient

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -2,22 +2,34 @@ import React from 'react';
 import { Root, Routes } from 'react-static';
 import { Router } from './components/Router/Router';
 import { Helmet } from 'react-helmet';
+import { ClientContext, GraphQLClient } from 'graphql-hooks';
+import memCache from 'graphql-hooks-memcache';
 import './app.css';
+
+const client = new GraphQLClient({
+  url: process.env.REACT_APP_GRAPHCMS_URL,
+  cache: memCache(),
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
+  }
+});
 
 const App = () => {
   return (
-    <Root>
-      <Helmet>
-        <title>Kimaleen Tran</title>
-      </Helmet>
-      <React.Suspense fallback={<em>Loading...</em>}>
-        <div className="content">
-          <Router>
-            <Routes path="*" />
-          </Router>
-        </div>
-      </React.Suspense>
-    </Root>
+    <ClientContext.Provider value={client}>
+      <Root>
+        <Helmet>
+          <title>Kimaleen Tran</title>
+        </Helmet>
+        <React.Suspense fallback={<em>Loading...</em>}>
+          <div className="content">
+            <Router>
+              <Routes path="*" />
+            </Router>
+          </div>
+        </React.Suspense>
+      </Root>
+    </ClientContext.Provider>
   );
 };
 
