437f1762aebf5624d0764d2256b6d918ecb81b5c
kurt miller <kmiller9393@gmail.com>
2019-08-26 21:37:07 -0600

    Change EmailForm.js to functional component

--- src/frontend/components/EmailForm/EmailForm.js
+++ src/frontend/components/EmailForm/EmailForm.js
@@ -1,40 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
+import FormValidation from '../FormValidation/FormValidation';
+import validateAuth from '../../utils/validateAuth';
 import './EmailForm.css';
 
-export default class EmailForm extends Component {
-  constructor() {
-    super();
-    this.state = {
-      email: ''
-    };
-  }
+const INITIAL_STATE = {
+  email: ''
+};
 
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
+const EmailForm = () => {
+  const {
+    handleSubmit,
+    handleChange,
+    handleBlur,
+    value,
+    errors,
+    isSubmitting
+  } = FormValidation(INITIAL_STATE, validateAuth);
 
-  handleSubmit = e => {
-    e.preventDefault();
-    const { email } = this.state;
+  return (
+    <form className="email-form" onSubmit={handleSubmit}>
+      <input
+        autoComplete="off"
+        className={errors.email ? 'email-input error-input' : 'email-input'}
+        name="email"
+        value={value.email}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        placeholder="Email Address"
+      />
+      <button
+        disabled={isSubmitting}
+        type="submit"
+        className="subscribe-button"
+      >
+        Subscribe!
+      </button>
+    </form>
+  );
+};
 
-    this.props.addIdea(email);
-  };
-
-  render() {
-    const { email } = this.state;
-
-    return (
-      <form className="email-form" onSubmit={this.handleSubmit}>
-        <input
-          className="email-input"
-          name="email"
-          value={email}
-          onChange={this.handleChange}
-          placeholder="Email Address"
-        />
-        <button className="subscribe-button">Subscribe!</button>
-      </form>
-    );
-  }
-}
+export default EmailForm;
